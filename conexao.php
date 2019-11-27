01d03d40468553cede17d152fffb97c0e1d3c18e
MaestroBruno <maestrobruno2000@gmail.com>
2019-11-27 10:07:59 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<? php
+SELECT * FROM `usuarios`
+		$servidor ="";
+		$usuario ="root";
+		$senha ="";
+		$dbname ="cadastro";
+		
+
+			$conn = mysql_connect($servidor, $usuario, $senha, $dbname );
+?>
